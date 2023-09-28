cd950ec4ee8c93c475eae710bb64fe34c1cf095d
Kathrin020 <katy-lola@mail.ru>
2023-09-28 22:04:59 +0300

    change

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const server = http.createServer((request, response) => {
   const params = url.searchParams;
 
   if (params.toString() !== "") {
-    if (params.has("Hello")) {
-      const name = params.get("Hello");
+    if (params.has("hello")) {
+      const name = params.get("hello");
       if (name) {
         response.statusCode = 200;
         response.setHeader("Content-Type", "text/plain");
@@ -15,25 +15,26 @@ const server = http.createServer((request, response) => {
         return;
       } else {
         response.statusCode = 400;
-        res.setHeader("Content-Type", "text/plain");
+        response.setHeader("Content-Type", "text/plain");
         response.end("Enter a name");
         return;
       }
+    }
+    if (url.pathname === "/users") {
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "application/json");
+      response.write(getUsers());
+      return;
     } else {
-      response.statusCode = 500;
-      response.end();
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/plain");
+      response.end("Hello,world!");
       return;
     }
+  } else {
+    response.statusCode = 500;
+    response.end();
   }
-  if (url.pathname === "/users") {
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "application/json");
-    response.end(getUsers());
-    return;
-  }
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "text/plain");
-  response.end("Hello,world!");
 });
 server.listen(3003, () => {
   console.log("Сервер запущен по адресу http://127.0.0.1:3003");
--- src/modules/users.js
+++ src/modules/users.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const getUsers = () => {
-  const filePath = path.join(__dirname, "../date/users.json");
+  const filePath = path.join(__dirname, "../date/users/json");
   return fs.readFileSync(filePath);
 };
 
